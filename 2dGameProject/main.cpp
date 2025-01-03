842ce29ac4c4b8a8a601117696470713833346a3
piwalker <piwalker@DESKTOP-P3K2VH5>
2025-01-03 11:03:45 -0700

    Finalized spdlog setup.

--- 2dGameProject/main.cpp
+++ 2dGameProject/main.cpp
@@ -4,18 +4,42 @@
 #include <GLFW/glfw3.h>
 #include "Window.h"
 #include <glad/glad.h>
+#include <glm/vec2.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <memory>
 
 int OpenGLVersion;
+spdlog::logger logger("none");
+
+#define TRACE(...) logger.trace(__VA_ARGS__)
+#define DEBUG(...) logger.debug(__VA_ARGS__)
+#define INFO(...) logger.info(__VA_ARGS__)
+#define WARN(...) logger.warn(__VA_ARGS__)
+#define ERROR(...) logger.error(__VA_ARGS__)
+#define CRITICAL(...) logger.critical(__VA_ARGS__)
 
 void init() {
-	if (!glfwInit())
+	auto console = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+	auto file = std::make_shared<spdlog::sinks::basic_file_sink_mt>("log.txt", true);
+	logger = spdlog::logger("Logger", {console, file});
+	INFO("spdlog Initialized");
+
+	if (!glfwInit()) {
+		ERROR("Unable to Initialize GLFW");
 		throw ("Unable to init GLFW");
+	}
+	INFO("GLFW Initialized!");
 }
 
 void openGLContextInit() {
 	OpenGLVersion = gladLoadGL();
-	if (OpenGLVersion == 0)
+	if (OpenGLVersion == 0) {
+		ERROR("Unable to load GLAD");
 		throw ("Failed to load GLAD");
+	}
+	INFO("GLAD Initialized");
 }
 
 void deinit() {
@@ -30,6 +54,7 @@ int main() {
 
 	window->beginRender();
 	openGLContextInit();
+	glm::vec2 vector(4, 3);
 
 	while (!window->isClosing()) {
 		glClear(GL_COLOR_BUFFER_BIT);
