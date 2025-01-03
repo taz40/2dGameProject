9e6406fc59492ac0fb73919b1c5399824fcedad7
piwalker <piwalker@DESKTOP-P3K2VH5>
2025-01-03 08:41:58 -0700

    Included GLAD for OpenGL support

--- 2dGameProject/main.cpp
+++ 2dGameProject/main.cpp
@@ -1,10 +1,21 @@
+#define GLFW_INCLUDE_NONE
+
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include "Window.h"
+#include <glad/glad.h>
+
+int OpenGLVersion;
 
 void init() {
 	if (!glfwInit())
 		throw ("Unable to init GLFW");
+}
+
+void openGLContextInit() {
+	OpenGLVersion = gladLoadGL();
+	if (OpenGLVersion == 0)
+		throw ("Failed to load GLAD");
 }
 
 void deinit() {
@@ -18,6 +29,7 @@ int main() {
 	Window* window = new Window(640, 480, "Test Window");
 
 	window->beginRender();
+	openGLContextInit();
 
 	while (!window->isClosing()) {
 		glClear(GL_COLOR_BUFFER_BIT);
