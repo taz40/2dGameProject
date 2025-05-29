beaf0674a1930037006249ff7a07643b9e09c42c
Samuel Walker <swalker@piwalker.net>
2025-05-28 18:05:37 -0600

    centered sprites

--- 2dGameProject/SpriteRenderer.cpp
+++ 2dGameProject/SpriteRenderer.cpp
@@ -40,10 +40,10 @@ SpriteRenderer::SpriteRenderer() {
 
 	//Initialize Resources
 	float vertexData[] = {
-		0.0f, 0.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
-		1.0f, 1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f
 	};
 
 	unsigned int indices[]{
